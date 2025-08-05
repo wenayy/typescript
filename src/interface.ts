05734842daf2edfd1a54d02e24d4e37c555ae108
vinay joshi <vinay@vinay-2.local>
2025-08-05 11:57:58 +0530

    interface added

---
+++ src/interface.ts
@@ -0,0 +1,13 @@
+interface User{
+    name:string,
+    age:number,
+    mature:boolean
+}
+
+let vinay:User= {
+    name:"vinayjoshi",
+    age:18,
+    mature:true
+}
+
+console.log(vinay.mature);
